78f2bd0c84b3875b9bc08edc1e14d8ee651efe3d
SDRodriguez10 <Sebastian.Rodriguez.1300@gmail.com>
2026-03-07 03:48:37 -0500

    Updating main to generate data file

--- src/feed_log.py
+++ src/feed_log.py
@@ -5,18 +5,16 @@ from pathlib import Path
 import csv
 
 class FeedLog:
-  def __init__(self):
+    def __init__(self):
     # init filename and location
     self.file_name = "feed_log.csv"
     self.file_loc  = "./data"
-    
     file_path = Path(self.file_loc + "/" + self.file_name)
+    
     if file_path.isfile():
-	with open(path, mode='r', newline='', encoding='utf-8') as file:
-	    csv_reader = csv.reader(file, delimiter=',')
-	    for row in csv_reader:
-		print(row)
-
+        print("Hello World")
+    else:
+        os.system("mkdir data/")
 
 
 
---
+++ src/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+
+if __name__ == "__main__":
+    data_loc = "~/.baby_data/data"
+    ex_path = os.path.expanduser(data_loc) # expanded path because ~
+    if os.path.exists(ex_path):
+        print("Hello World")
+    else:
+        print("oh noooo")
+        os.system("mkdir ~/.baby_data")
+        os.system("mkdir ~/.baby_data/data")
+
