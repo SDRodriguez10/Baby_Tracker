523cfc7ff72cf1bb02c39afebbe628e38b6266c1
SDRodriguez10 <Sebastian.Rodriguez.1300@gmail.com>
2026-03-07 17:02:58 -0500

    FeedLog class now builds and gets the file information

--- main.py
+++ main.py
@@ -2,35 +2,7 @@
 import os
 import csv
 from src import *
-def get_data(dir_path, file_names):
-    for fi_name in file_names:
-        glued_path = dir_path + "/" + fi_name
-        ex_path = os.path.expanduser(glued_path)
-        if (os.path.exists(ex_path)):
-            print(str(fi_name) + " found! Importing existing data")
-            with open(ex_path, mode='r', newline='', encoding='utf-8') as csv_file:
-                dict_reader = csv.DictReader(csv_file)
-                for row in dict_reader:
-                    feed_list.append(row)
-                    
-        else:
-            print(".baby_data not found! Generating new data folder")
-            os.system("mkdir ~/.baby_data")
-            os.system("touch ~/.baby_data/data.csv")
-    return 0
-
-def gen_files(dir_path, file_names):
-    for fi_name in file_names:
-        
-        # cat the file name and dir path together
-        glued_path = dir_path + "/" + fi_name
-        # Expand the ~/ part 
-        ex_path = os.path.expanduser(glued_path)
-
-        # Create empty file if it does not exist
-        open(ex_path, "a").close()
-    return 0
-
+from feed_log import FeedLog
 
 if __name__ == "__main__":
     file_names = ["feedLog.csv", "diaperLog.csv", "sleep_log.csv"]
@@ -40,10 +12,9 @@ if __name__ == "__main__":
     # Data storage lists
     data_lists = []
 
-    # If ~/.baby_data exists, get data from it, if not init it
+    # If ~/.baby_data exists cool, if not make it
     if (os.path.exists(ex_path)):
-        print("~/.baby_data found, extracting data\n")
-        data_lists = get_data(ex_path, file_names)
+        print("~/.baby_data found\n")
     else:
         print("~/.baby_data not found, creating it now")
         os.system("mkdir ~/.baby_data")
--- src/feed_log.py
+++ src/feed_log.py
@@ -1,6 +1,5 @@
 # This is the class that manages the feeding schedule add/update/remove
 
-import numpy as np
 from pathlib import Path
 import csv
 
@@ -10,13 +9,30 @@ class FeedLog:
         self.file_name = "feed_log.csv"
         self.file_loc  = "~/.baby_data"
         # Check if file exists
-
-        self.data_path = gen_path()
+        self.data_path = ""
+        self.feed_list = []
+        gen_path()
+        set_data()
+        get_data()
 
     def gen_path(self):
-        # If this is the first time running then gen the file
-        glued_path = self.file_loc + "/" + self.file_loc
+        # If this is the first time running then gen the file path
+        glued_path = self.file_loc + "/" + self.file_name
+        self.data_path = os.path.expanduser(glued_path)
+        # If the file doesn't exist make it, if it does leave it alone
+        open(self.data_path, "a").close
+        return 0
+
+    def set_data(self):
+        if (os.path.exists(self.data_path)):
+            print(std(self.file_name) + " found! Importing existing data")
+            with open(self.data_path, mode='r', newline='', encoding='utf-8') as csv_file:
+                dict_reader = csv.DictReader(csv_file)
+                for row in dict_reader:
+                    self.feed_list.append(row)
         return 0
 
     def get_data(self):
+        for row in self.feed_list:
+            print(row)
         return 0
